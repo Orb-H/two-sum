b0b55859cf1b7adb384d7518a15448060d21f87b
Orb-H <orb.cov@gmail.com>
2023-03-09 19:30:20 +0900

    Add main.py with twoSum()

---
+++ main.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    # Perhaps there is no answer
+    return [0, 1]
